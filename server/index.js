9801578d48427e56bf2ccac6b7549481b1f79113
Jonathan Tang <j.tang17@gmail.com>
2017-11-16 20:04:58 -0800

    changed a bit

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const db = require('../database/index.js');
-//const config = require('../config.js');
+
 const session = require('express-session');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
